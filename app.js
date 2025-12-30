0c9c27bc051f8b7ad4bfce4c92eab16626aa03a5
Akshay Kumar <akshaykumar880466@gmail.com>
2025-12-31 02:42:30 +0530

    install Ts

---
+++ app.js
@@ -0,0 +1,21 @@
+var a = "Akshay";
+var a1 = 42;
+console.log(a);
+a = "Akshay Yadav";
+console.log(a);
+// 🔹 What is TypeScript?
+// Definition
+// TypeScript = JavaScript + Types
+// It is a superset of JavaScript developed by Microsoft.
+// 👉 Any JavaScript code is valid TypeScript
+// 👉 TypeScript adds type safety
+// Why TypeScript?
+// Finds errors before runtime
+// Better code readability
+// Best for large projects
+// Industry standard for React, Next.js, Node.j
+// 🔹 How TypeScript Works
+// You write .ts code
+// TypeScript compiles it to .js
+// Browser / Node runs JavaScript
+// TypeScript (.ts) → Compiler → JavaScript (.js)
---
+++ app.ts
@@ -0,0 +1,50 @@
+var a:string ="Akshay"
+var a1:number=42
+
+console.log(a)
+
+a="Akshay Yadav"
+
+console.log(a)
+
+
+
+
+
+// 🔹 What is TypeScript?
+// Definition
+
+// TypeScript = JavaScript + Types
+
+// It is a superset of JavaScript developed by Microsoft.
+
+// 👉 Any JavaScript code is valid TypeScript
+// 👉 TypeScript adds type safety
+
+// Why TypeScript?
+
+// Finds errors before runtime
+
+// Better code readability
+
+// Best for large projects
+
+// Industry standard for React, Next.js, Node.j
+
+
+
+
+
+
+
+
+
+// 🔹 How TypeScript Works
+
+// You write .ts code
+
+// TypeScript compiles it to .js
+
+// Browser / Node runs JavaScript
+
+// TypeScript (.ts) → Compiler → JavaScript (.js)
